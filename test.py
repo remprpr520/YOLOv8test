4b481e021919d93499194b7f245a318abfcb1529
remprpr520 <1449963079@qq.com>
2025-03-14 21:17:00 +0800

    1

--- test.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.yaml")
-model.train(data="coco128.yaml", epochs=5)
-model.val()  # It'll automatically evaluate the data you trained.
--- train.py
+++ train.py
@@ -2,3 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt") # pass any model type
 model.train(epochs=5)
+model.val()  # It'll automatically evaluate the data you trained.
